3cf32327bcb3ba17eb31f50a1dc2b1a5bf60c6bb
AdluAghnia <adluaghnia03@gmail.com>
2023-09-03 21:20:43 +0700

    mindahin table sama nambah table

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
         Player2: document.getElementById('value-display_player2')
     };
     const tableBody = document.getElementById('mark-table-body');
-
+    const tableBody2 = document.getElementById('mark-table-body-2');
     function uppercutHandler(player) {
         return function() {
             marking = true;
@@ -36,14 +36,34 @@ document.addEventListener('DOMContentLoaded', function() {
             circle.style.top = `${y - 10}px`;
             imageDiv.appendChild(circle);
 
-            coordinateDisplays[currentPlayer].innerHTML = `Coordinates: (${x}, ${y})`;
+            coordinateDisplays[currentPlayer].innerHTML = `Coordinates: (${x.toFixed(2)}, ${y.toFixed(2)})`;
 
-            const newRow = tableBody.insertRow();
-            newRow.insertCell(0).innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
-            newRow.insertCell(1).innerHTML = 'Uppercut';
-            newRow.insertCell(2).innerHTML = currentPlayer;
+            if (currentPlayer === 'Player') {
+              const newRow = tableBody.insertRow();
+              const Player1_cell1 = newRow.insertCell(0);
+              const Player1_cell2 = newRow.insertCell(1);
+              const Player1_cell3 = newRow.insertCell(2);
 
-            marking = false;
+              Player1_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
+              Player1_cell2.innerHTML = 'Uppercut';
+              Player1_cell3.innerHTML = currentPlayer;
+
+              marking = false;
+            }
+            else if (currentPlayer === 'Player2')
+            {
+              const newRow = tableBody2.insertRow();
+              const Player2_cell1 = newRow.insertCell(0);
+              const Player2_cell2 = newRow.insertCell(1);
+              const Player2_cell3 = newRow.insertCell(2);
+              
+
+              Player2_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`
+              Player2_cell2.innerHTML = 'Uppercut';
+              Player2_cell3.innerHTML = currentPlayer;
+
+              marking = false;
+            }
         }
     });
 });
