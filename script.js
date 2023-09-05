6f9b618b0284dda2c9beec57f5b9a750a3759322
AdluAghnia <adluaghnia03@gmail.com>
2023-09-05 18:53:33 +0700

    misahin gambar dll.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentPlayer = "";
 
     const imageDiv = document.getElementById('image');
+    const imageDiv2 = document.getElementById('image2')
     const coordinateDisplays = {
         Player: document.getElementById('coordinate-display'),
         Player2: document.getElementById('coordinate-display_player2')
@@ -12,7 +13,8 @@ document.addEventListener('DOMContentLoaded', function() {
         Player2: document.getElementById('value-display_player2')
     };
     const tableBody = document.getElementById('mark-table-body');
-    const tableBody2 = document.getElementById('mark-table-body-2');
+    const tableBody2 = document.getElementById('mark-table-body2');
+
     function uppercutHandler(player) {
         return function() {
             marking = true;
@@ -37,33 +39,48 @@ document.addEventListener('DOMContentLoaded', function() {
             imageDiv.appendChild(circle);
 
             coordinateDisplays[currentPlayer].innerHTML = `Coordinates: (${x.toFixed(2)}, ${y.toFixed(2)})`;
+            if (currentPlayer === 'Player1') {
+                const newRow = tableBody.insertRow();
+                const Player1_cell1 = newRow.insertCell(0);
+                const Player1_cell2 = newRow.insertCell(1);
+                const Player1_cell3 = newRow.insertCell(2);
 
-            if (currentPlayer === 'Player') {
-              const newRow = tableBody.insertRow();
-              const Player1_cell1 = newRow.insertCell(0);
-              const Player1_cell2 = newRow.insertCell(1);
-              const Player1_cell3 = newRow.insertCell(2);
+                Player1_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
+                Player1_cell2.innerHTML = 'Uppercut';
+                Player1_cell3.innerHTML = currentPlayer;
 
-              Player1_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
-              Player1_cell2.innerHTML = 'Uppercut';
-              Player1_cell3.innerHTML = currentPlayer;
-
-              marking = false;
             }
-            else if (currentPlayer === 'Player2')
-            {
-              const newRow = tableBody2.insertRow();
+        }
+    });
+
+    // Tambahkan event listener ke elemen gambar
+    imageDiv2.addEventListener('click', function(event) {
+        if (marking) {
+            const rect = imageDiv.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+
+            const circle = document.createElement('div');
+            circle.classList.add('mark-circle');
+            circle.style.left = `${x - 10}px`;
+            circle.style.top = `${y - 10}px`;
+            imageDiv.appendChild(circle);
+
+            coordinateDisplays[currentPlayer].innerHTML = `Coordinates: (${x.toFixed(2)}, ${y.toFixed(2)})`;
+            if (currentPlayer === 'Player2') {
+              const newRow = tableBody.insertRow();
+
+
               const Player2_cell1 = newRow.insertCell(0);
               const Player2_cell2 = newRow.insertCell(1);
               const Player2_cell3 = newRow.insertCell(2);
-              
 
-              Player2_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`
+              Player2_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
               Player2_cell2.innerHTML = 'Uppercut';
               Player2_cell3.innerHTML = currentPlayer;
-
               marking = false;
             }
         }
     });
 });
+
