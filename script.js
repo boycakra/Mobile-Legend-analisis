e3a843c2cffe82d3c067117e60000558d5ff6cdf
Aldi Ananda Prasetya <anandhaaldi11249@gmail.com>
2023-09-07 20:27:56 +0700

    fix:table scroll & mark-circle

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
     let currentPlayer = "";
 
     // Ambil ID dari image
-    const imageDiv = document.querySelector("#image-container");
-    // const imageDiv = document.getElementById("image");
+    const imageDiv = document.querySelector("#image-container #image1");
+    const imageDiv2 = document.querySelector("#image-container #image2");
+    // const imageDiv = document.getElementById("image1");
     // const imageDiv2 = document.getElementById("image2");
 
     // Tampilkan hasil titik kordinat
     const coordinateDisplays = {
         Player: document.getElementById("coordinate-display"),
-        // Player2: document.getElementById("coordinate-display_player2"),
+        Player2: document.getElementById("coordinate-display_player2"),
     };
 
     // Tampilkan value
     const valueDisplays = {
         Player: document.getElementById("value-display"),
-        // Player2: document.getElementById("value-display_player2"),
+        Player2: document.getElementById("value-display_player2"),
     };
 
     // Penangkap reaksi tombol uppercut
@@ -29,6 +30,16 @@ document.addEventListener("DOMContentLoaded", function () {
         };
     }
 
+    function printMousePos(e) {
+        const event = window.event;
+        const test = document.createElement("div");
+        test.setAttribute(
+            "style",
+            `position:absolute; left:${event.pageX}px; top:${event.pageY}px; background-color:red; color:red; width:10px; height:10px; z-index:20; font-size:2em; border-radius: 50%;`
+        );
+        document.body.appendChild(test);
+    }
+
     // Tambahkan event listener ke elemen gambar
     document
         .getElementById("uppercut")
@@ -39,17 +50,10 @@ document.addEventListener("DOMContentLoaded", function () {
     imageDiv.addEventListener("click", function (event) {
         if (marking) {
             const rect = imageDiv.getBoundingClientRect();
-            console.info(rect);
             const x = event.clientX - rect.left;
             const y = event.clientY - rect.top;
 
-            console.table(x, y);
-
-            const circle = document.createElement("div");
-            circle.classList.add("mark-circle");
-            circle.style.left = `${x - 10}px`;
-            circle.style.top = `${y - 10}px`;
-            imageDiv.parentElement.appendChild(circle);
+            printMousePos(imageDiv);
 
             coordinateDisplays[
                 currentPlayer
@@ -70,40 +74,36 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // // Tambahkan event listener ke elemen gambar
-    // document
-    //     .getElementById("uppercutplayer2")
-    //     .addEventListener("click", uppercutHandler("Player2"));
-    // // Ambil lokasi table
-    // const tableBody2 = document.getElementById("mark-table-body-2");
-    // // kirim kordinat pada table
-    // imageDiv2.addEventListener("click", function (event) {
-    //     if (marking) {
-    //         const rect = imageDiv.getBoundingClientRect();
-    //         const x = event.clientX - rect.left;
-    //         const y = event.clientY - rect.top;
+    document
+        .getElementById("uppercutplayer2")
+        .addEventListener("click", uppercutHandler("Player2"));
+    // Ambil lokasi table
+    const tableBody2 = document.getElementById("mark-table-body-2");
+    // kirim kordinat pada table
+    imageDiv2.addEventListener("click", function (event) {
+        if (marking) {
+            const rect = imageDiv.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
 
-    //         const circle = document.createElement("div");
-    //         circle.classList.add("mark-circle");
-    //         circle.style.left = `${x - 10}px`;
-    //         circle.style.top = `${y - 10}px`;
-    //         imageDiv.appendChild(circle);
+            printMousePos(imageDiv);
 
-    //         coordinateDisplays[
-    //             currentPlayer
-    //         ].innerHTML = `Coordinates: (${x.toFixed(2)}, ${y.toFixed(2)})`;
+            coordinateDisplays[
+                currentPlayer
+            ].innerHTML = `Coordinates: (${x.toFixed(2)}, ${y.toFixed(2)})`;
 
-    //         if (currentPlayer === "Player2") {
-    //             const newRow = tableBody2.insertRow();
+            if (currentPlayer === "Player2") {
+                const newRow = tableBody2.insertRow();
 
-    //             const Player2_cell1 = newRow.insertCell(0);
-    //             const Player2_cell2 = newRow.insertCell(1);
-    //             const Player2_cell3 = newRow.insertCell(2);
+                const Player2_cell1 = newRow.insertCell(0);
+                const Player2_cell2 = newRow.insertCell(1);
+                const Player2_cell3 = newRow.insertCell(2);
 
-    //             Player2_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
-    //             Player2_cell2.innerHTML = "Uppercut";
-    //             Player2_cell3.innerHTML = currentPlayer;
-    //             marking = false;
-    //         }
-    //     }
-    // });
+                Player2_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
+                Player2_cell2.innerHTML = "Uppercut";
+                Player2_cell3.innerHTML = currentPlayer;
+                marking = false;
+            }
+        }
+    });
 });
