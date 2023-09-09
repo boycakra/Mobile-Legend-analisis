1dc790e8b32421f847bed847c2c9e9b053eb166e
boycakra <boycakra69@gmail.com>
2023-09-09 18:25:46 +0700

    Make new Tamplate
    New 3 new button, new css mark
    jab and uppercut fail

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     let marking = false;
     let currentPlayer = "";
+    let markingCircleClass = "mark-circle"; // By default
 
     // Ambil ID dari image
     const imageDiv = document.querySelector("#image-container #image1");
     const imageDiv2 = document.querySelector("#image-container #image2");
-    // const imageDiv = document.getElementById("image1");
-    // const imageDiv2 = document.getElementById("image2");
 
     // Tampilkan hasil titik kordinat
     const coordinateDisplays = {
@@ -20,90 +19,121 @@ document.addEventListener("DOMContentLoaded", function () {
         Player2: document.getElementById("value-display_player2"),
     };
 
-    // Penangkap reaksi tombol uppercut
+    function printMousePos(event, element) {
+        const test = document.createElement("div");
+    
+        // Using clientX and clientY to get coordinates relative to viewport
+        const x = event.clientX; 
+        const y = event.clientY;
+    
+        test.style.left = `${x}px`;
+        test.style.top = `${y}px`;
+        test.style.position = 'absolute';
+        test.style.zIndex = 20;
+        test.style.width = '20px';
+        test.style.height = '20px';
+        test.style.borderRadius = '50%';
+    
+        // Apply the appropriate class styles based on markingCircleClass
+        if (markingCircleClass === 'mark-circle') {
+            test.style.background = 'red';
+        } else if (markingCircleClass === 'jab-mark-circle') {
+            test.style.background = 'blue';
+        } else if (markingCircleClass === 'mark-innercircle') {
+            test.style.background = 'radial-gradient(circle, red 40%, transparent 40%)';
+            test.style.border = '2px solid red';
+        }
+    
+        document.body.appendChild(test);
+    }
+    
+
     function uppercutHandler(player) {
-        console.log("Masuk Uppercut Handler");
         return function () {
             marking = true;
             currentPlayer = player;
+            markingCircleClass = 'mark-circle';
             valueDisplays[player].innerHTML = "Value: Uppercut";
         };
     }
 
-    function printMousePos(e) {
-        const event = window.event;
-        const test = document.createElement("div");
-        test.setAttribute(
-            "style",
-            `position:absolute; left:${event.pageX}px; top:${event.pageY}px; background-color:red; color:red; width:10px; height:10px; z-index:20; font-size:2em; border-radius: 50%;`
-        );
-        document.body.appendChild(test);
+    function jabHandler(player) {
+        return function () {
+            marking = true;
+            currentPlayer = player;
+            markingCircleClass = 'jab-mark-circle';
+            valueDisplays[player].innerHTML = "Value: Jab";
+        };
     }
 
-    // Tambahkan event listener ke elemen gambar
-    document
-        .getElementById("uppercut")
-        .addEventListener("click", uppercutHandler("Player"));
+    function uppercutfHandler(player) {
+        return function () {
+            marking = true;
+            currentPlayer = player;
+            markingCircleClass = 'mark-innercircle';
+            valueDisplays[player].innerHTML = "Value: UppercutF";
+        };
+    }
+
+    // Button click listeners for Player 1
+    document.getElementById('uppercut').addEventListener('click', uppercutHandler('Player'));
+    document.getElementById('UppercutF').addEventListener('click', uppercutfHandler('Player'));
+    document.getElementById('jab').addEventListener('click', jabHandler('Player'));
+
+    // Button click listeners for Player 2
+    document.getElementById('uppercutplayer2').addEventListener('click', uppercutHandler('Player2'));
+    document.getElementById('UppercutFp2').addEventListener('click', uppercutfHandler('Player2'));
+    document.getElementById('jabplayer2').addEventListener('click', jabHandler('Player2'));
+
     // Ambil lokasi table
     const tableBody1 = document.getElementById("mark-table-body");
-    // kirim kordinat pada table
+    const tableBody2 = document.getElementById("mark-table-body-2");
+
+    // Event listener for image click (Player 1)
     imageDiv.addEventListener("click", function (event) {
-        if (marking) {
+        if (marking && (currentPlayer === "Player")) {
             const rect = imageDiv.getBoundingClientRect();
             const x = event.clientX - rect.left;
             const y = event.clientY - rect.top;
 
-            printMousePos(imageDiv);
+            printMousePos(event, imageDiv);
 
-            coordinateDisplays[
-                currentPlayer
-            ].innerHTML = `Coordinates: (${x.toFixed(2)}, ${y.toFixed(2)})`;
+            coordinateDisplays[currentPlayer].innerHTML = `Coordinates: (${x.toFixed(2)}, ${y.toFixed(2)})`;
             marking = false;
 
-            if (currentPlayer === "Player") {
-                const newRow = tableBody1.insertRow();
-                const Player1_cell1 = newRow.insertCell(0);
-                const Player1_cell2 = newRow.insertCell(1);
-                const Player1_cell3 = newRow.insertCell(2);
+            // Add data to table for Player
+            const newRow = tableBody1.insertRow();
+            const Player1_cell1 = newRow.insertCell(0);
+            const Player1_cell2 = newRow.insertCell(1);
+            const Player1_cell3 = newRow.insertCell(2);
 
-                Player1_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
-                Player1_cell2.innerHTML = "Uppercut";
-                Player1_cell3.innerHTML = currentPlayer;
-            }
+            Player1_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
+            Player1_cell2.innerHTML = valueDisplays[currentPlayer].innerText.split(": ")[1];
+            Player1_cell3.innerHTML = currentPlayer;
         }
     });
 
-    // // Tambahkan event listener ke elemen gambar
-    document
-        .getElementById("uppercutplayer2")
-        .addEventListener("click", uppercutHandler("Player2"));
-    // Ambil lokasi table
-    const tableBody2 = document.getElementById("mark-table-body-2");
-    // kirim kordinat pada table
+    // Event listener for image click (Player 2)
     imageDiv2.addEventListener("click", function (event) {
-        if (marking) {
-            const rect = imageDiv.getBoundingClientRect();
+        if (marking && (currentPlayer === "Player2")) {
+            const rect = imageDiv2.getBoundingClientRect();
             const x = event.clientX - rect.left;
             const y = event.clientY - rect.top;
 
-            printMousePos(imageDiv);
+            printMousePos(event, imageDiv2);
 
-            coordinateDisplays[
-                currentPlayer
-            ].innerHTML = `Coordinates: (${x.toFixed(2)}, ${y.toFixed(2)})`;
+            coordinateDisplays[currentPlayer].innerHTML = `Coordinates: (${x.toFixed(2)}, ${y.toFixed(2)})`;
+            marking = false;
 
-            if (currentPlayer === "Player2") {
-                const newRow = tableBody2.insertRow();
+            // Add data to table for Player2
+            const newRow = tableBody2.insertRow();
+            const Player2_cell1 = newRow.insertCell(0);
+            const Player2_cell2 = newRow.insertCell(1);
+            const Player2_cell3 = newRow.insertCell(2);
 
-                const Player2_cell1 = newRow.insertCell(0);
-                const Player2_cell2 = newRow.insertCell(1);
-                const Player2_cell3 = newRow.insertCell(2);
-
-                Player2_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
-                Player2_cell2.innerHTML = "Uppercut";
-                Player2_cell3.innerHTML = currentPlayer;
-                marking = false;
-            }
+            Player2_cell1.innerHTML = `${x.toFixed(2)}, ${y.toFixed(2)}`;
+            Player2_cell2.innerHTML = valueDisplays[currentPlayer].innerText.split(": ")[1];
+            Player2_cell3.innerHTML = currentPlayer;
         }
     });
 });
