87e02e3d3552b7bdfc538a76f30e090594f4f350
boycakra <boycakra69@gmail.com>
2023-09-12 19:21:34 +0700

    add mark button and add timer
    add mark button and add timer

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
         Player: document.getElementById("value-display"),
         Player2: document.getElementById("value-display_player2"),
     };
+    // Add the timer display element
+    const timerDisplay = document.getElementById("timer-display");
 
     function printMousePos(event, element) {
         const circle = document.createElement("div");
@@ -43,6 +45,10 @@ document.addEventListener("DOMContentLoaded", function () {
             circle.style.background = 'radial-gradient(circle, red 40%, transparent 40%)';
             circle.style.border = '2px solid red';
         }
+        else if (markingCircleClass === 'jabf-mark-innercircle') {
+            circle.style.background = 'radial-gradient(circle, blue 40%, transparent 40%)';
+            circle.style.border = '2px solid blue';
+        }
     
         document.body.appendChild(circle);
     }
@@ -54,6 +60,15 @@ document.addEventListener("DOMContentLoaded", function () {
             currentPlayer = player;
             markingCircleClass = 'mark-circle';
             valueDisplays[player].innerHTML = "Value: Uppercut";
+        };
+    }
+
+    function jabfHandler(player) {
+        return function () {
+            marking = true;
+            currentPlayer = player;
+            markingCircleClass = 'jabf-mark-innercircle';
+            valueDisplays[player].innerHTML = "Value: Jabf";
         };
     }
 
@@ -74,16 +89,24 @@ document.addEventListener("DOMContentLoaded", function () {
             valueDisplays[player].innerHTML = "Value: UppercutF";
         };
     }
+    function updateTimer(video) {
+        const minutes = Math.floor(video.currentTime / 60);
+        const seconds = Math.floor(video.currentTime % 60);
+        const formattedTime = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+        timerDisplay.textContent = formattedTime;
+    }
 
     // Button click listeners for Player 1
     document.getElementById('uppercut').addEventListener('click', uppercutHandler('Player'));
     document.getElementById('UppercutF').addEventListener('click', uppercutfHandler('Player'));
     document.getElementById('jab').addEventListener('click', jabHandler('Player'));
+    document.getElementById('jabf').addEventListener('click', jabfHandler('Player'));
 
     // Button click listeners for Player 2
     document.getElementById('uppercutplayer2').addEventListener('click', uppercutHandler('Player2'));
     document.getElementById('UppercutFp2').addEventListener('click', uppercutfHandler('Player2'));
     document.getElementById('jabplayer2').addEventListener('click', jabHandler('Player2'));
+    document.getElementById('jabfp2').addEventListener('click', jabfHandler('Player2'));
 
     // Ambil lokasi table
     const tableBody1 = document.getElementById("mark-table-body");
@@ -135,5 +158,10 @@ document.addEventListener("DOMContentLoaded", function () {
             Player2_cell2.innerHTML = valueDisplays[currentPlayer].innerText.split(": ")[1];
             Player2_cell3.innerHTML = currentPlayer;
         }
+    });
+    // Add a timeupdate event listener to the video
+    const video = document.querySelector("#video-container video");
+    video.addEventListener("timeupdate", function () {
+        updateTimer(video);
     });
 });
