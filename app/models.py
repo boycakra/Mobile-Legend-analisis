53a4a0437dca6094888d991a4fe30e507f5613e5
AdluAghnia <adluaghnia03@gmail.com>
2023-12-17 11:52:34 +0700

    ADD : User profile picture

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique = True, nullable = False)
     email  = db.Column(db.String(200), unique = False, nullable = False)
+    image_file = db.Column(db.String(120), nullable = False, default = "profile.jpg")
     password = db.Column(db.String(60), nullable = False)
 
     video = db.relationship('Video', backref = 'author', lazy=True)
