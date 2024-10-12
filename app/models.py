3bf44c7a16e7ead9dbabcfef3fd1814ff21398ba
AdluAghnia <adluaghnia03@gmail.com>
2024-10-13 03:39:14 +0700

    Feat : Login Logic and JWT

--- app/models.py
+++ app/models.py
@@ -2,10 +2,13 @@ import uuid
 from app import db
 from hashlib import sha256
 
-class User(db.Model):
-    __tablename__ = 'users'
 
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()), unique=True)
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()), unique=True
+    )
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
@@ -14,11 +17,11 @@ class User(db.Model):
         self.id = str(uuid.uuid4())  # Automatically generate a UUID
         self.username = username
         self.email = email
-        self.password = sha256(password.encode('utf-8')).hexdigest()
+        self.password = sha256(password.encode("utf-8")).hexdigest()
 
     def check_password(self, password):
         """Verify the provided password against the hashed password stored in the database."""
-        hashed_password = sha256(password.encode('utf-8')).hexdigest()
+        hashed_password = sha256(password.encode("utf-8")).hexdigest()
         return self.password == hashed_password
 
     def __repr__(self):
